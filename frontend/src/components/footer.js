fb84b06783cd007519445a907fb734fcc8d4d035
Smita Mishra <smitamishra12341@gmail.com>
2021-12-16 20:47:18 +0530

    icon change

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -1,99 +1,130 @@
+import { NavLink } from "react-router-dom";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
 const Footer = () => {
-  return(
+  return (
     <div className="container-fluid  my-5">
+      <footer className="text-white text-center text-lg-start bg-dark">
+        <div className="container p-4">
+          <div className="row mt-4">
+            <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
+              <h5 className="text-uppercase mb-4">About company</h5>
+              <p>This is a Portfolio Building Web Application</p>
+              <p>Second Line</p>
+              <div className="mt-4">
+                <NavLink
+                  to="/"
+                  type="button"
+                  className="btn btn-floating  btn-lg"
+                >
+                  <FacebookIcon style={{fill: "green"}}/>
 
-  <footer className="text-white text-center text-lg-start bg-dark">
-    {/* <!-- Grid container --> */}
-    <div className="container p-4">
-      {/* <!--Grid row--> */}
-      <div className="row mt-4">
-        {/* <!--Grid column--> */}
-        <div className="col-lg-4 col-md-12 mb-4 mb-md-0">
-          <h5 className="text-uppercase mb-4">About company</h5>
+                </NavLink>
+                <NavLink
+                  to="/"
+                  type="button"
+                  className="btn btn-floating btn-lg"
+                >
+                  <InstagramIcon style={{fill: "green"}}/>
+                  
+                </NavLink>
+                <NavLink
+                  to="/"
+                  type="button"
+                  className="btn btn-floating  btn-lg"
+                >
+                  <LinkedInIcon style={{fill: "green"}} />
+                </NavLink>
+                <NavLink
+                  to="/"
+                  type="button"
+                  className="btn btn-floating  btn-lg"
+                >
+                  <TwitterIcon style={{fill: "green"}}/>
+                </NavLink>
+              </div>
+            </div>
 
-          <p>
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium
-            voluptatum deleniti atque corrupti.
-          </p>
+            <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
 
-          <p>
-            Blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas
-            molestias.
-          </p>
+              <div className="form-outline form-white mb-4">
+                <input
+                  type="text"
+                  id="formControlLg"
+                  className="form-control form-control-lg"
+                />
+                <label className="form-label" for="formControlLg">
+                  Search
+                </label>
+              </div>
 
-          <div className="mt-4">
-            {/* <!-- Facebook --> */}
-            <a type="button" className="btn btn-floating btn-light btn-lg"><i className="fab fa-facebook-f"></i></a>
-            {/* <!-- Dribbble --> */}
-            <a type="button" className="btn btn-floating btn-light btn-lg"><i className="fab fa-dribbble"></i></a>
-            {/* <!-- Twitter --> */}
-            <a type="button" className="btn btn-floating btn-light btn-lg"><i className="fab fa-twitter"></i></a>
-            {/* <!-- Google + --> */}
-            <a type="button" className="btn btn-floating btn-light btn-lg"><i className="fab fa-google-plus-g"></i></a>
-            {/* <!-- Linkedin --> */}
+              <ul className="fa-ul" style={{ marginLeft: "1.65em" }}>
+                <li className="mb-3">
+                  <span className="fa-li">
+                    <i className="fas fa-home"></i>
+                  </span>
+                  <span className="ms-2">Warsaw, 00-967, Poland</span>
+                </li>
+                <li className="mb-3">
+                  <span className="fa-li">
+                    <i className="fas fa-envelope"></i>
+                  </span>
+                  <span className="ms-2">contact@example.com</span>
+                </li>
+                <li className="mb-3">
+                  <span className="fa-li">
+                    <i className="fas fa-phone"></i>
+                  </span>
+                  <span className="ms-2">+ 48 234 567 88</span>
+                </li>
+              </ul>
+            </div>
+            {/* <!--Grid column--> */}
+
+            {/* <!--Grid column--> */}
+            <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
+              <h5 className="text-uppercase mb-4">Opening hours</h5>
+
+              <table className="table text-center text-white">
+                <tbody className="fw-normal">
+                  <tr>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  <tr>
+                    <td>Sunday:</td>
+                    <td>9am - 10pm</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            {/* <!--Grid column--> */}
           </div>
+          {/* <!--Grid row--> */}
         </div>
-        {/* <!--Grid column--> */}
+        {/* <!-- Grid container --> */}
 
-        {/* <!--Grid column--> */}
-        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase mb-4 pb-1">Search something</h5>
-
-          <div className="form-outline form-white mb-4">
-            <input type="text" id="formControlLg" className="form-control form-control-lg" />
-            <label className="form-label" for="formControlLg">Search</label>
-          </div>
-
-          <ul className="fa-ul" style={{marginLeft: '1.65em'}}>
-            <li className="mb-3">
-              <span className="fa-li"><i className="fas fa-home"></i></span><span className="ms-2">Warsaw, 00-967, Poland</span>
-            </li>
-            <li className="mb-3">
-              <span className="fa-li"><i className="fas fa-envelope"></i></span><span className="ms-2">contact@example.com</span>
-            </li>
-            <li className="mb-3">
-              <span className="fa-li"><i className="fas fa-phone"></i></span><span className="ms-2">+ 48 234 567 88</span>
-            </li>
-          </ul>
+        {/* <!-- Copyright --> */}
+        <div
+          className="text-center p-3"
+          style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+        >
+          © 2020 Copyright:
+          <a className="text-white" href="https://mdbootstrap.com/">
+            MDBootstrap.com
+          </a>
         </div>
-        {/* <!--Grid column--> */}
-
-        {/* <!--Grid column--> */}
-        <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
-          <h5 className="text-uppercase mb-4">Opening hours</h5>
-
-          <table className="table text-center text-white">
-            <tbody className="fw-normal">
-              <tr>
-                <td>Mon - Thu:</td>
-                <td>8am - 9pm</td>
-              </tr>
-              <tr>
-                <td>Fri - Sat:</td>
-                <td>8am - 1am</td>
-              </tr>
-              <tr>
-                <td>Sunday:</td>
-                <td>9am - 10pm</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        {/* <!--Grid column--> */}
-      </div>
-      {/* <!--Grid row--> */}
-    </div>
-    {/* <!-- Grid container --> */}
-
-    {/* <!-- Copyright --> */}
-    <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
-      © 2020 Copyright:
-      <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
-    </div>
-    {/* <!-- Copyright -->/ */}
+        {/* <!-- Copyright -->/ */}
       </footer>
-
-</div>
+    </div>
   );
 };
 export default Footer;
